f35b7112935a87f6f8d151e1092999483dfe90be
Bolum Victor <bolum.victor@EPAYPLUSNG.LOCAL>
2019-05-15 13:32:01 +0100

    call hub events from outside

---
+++ WebApplication1/Controllers/OutsideController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using WebApplication1.Hubs;
+
+namespace WebApplication1.Controllers
+{
+
+    public class OutsideController : Controller
+    {
+        private readonly IHubContext<ChatHub> hubContext;
+
+        //Inject an instance of IHubContext in a controller
+        public OutsideController(IHubContext<ChatHub> hubContext)
+        {
+            this.hubContext = hubContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            //send message from outside the hub
+            await hubContext.Clients.All.SendAsync("Notify", $"Home page loaded at: {DateTime.Now}");
+
+            return View();
+        }
+    }
+}
--- WebApplication1/Hubs/ChatHub.cs
+++ WebApplication1/Hubs/ChatHub.cs
@@ -8,10 +8,32 @@ namespace WebApplication1.Hubs
 {
     public class ChatHub : Hub
     {
+
+        public override async Task OnConnectedAsync()
+        {
+            var connId = Context.ConnectionId;
+            await Groups.AddToGroupAsync(Context.ConnectionId, "SignalR Users");
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, "SignalR Users");
+            await base.OnDisconnectedAsync(exception);
+        }
+
         public async Task SendMessage(string user, string message)
         {
             //Emits ReceiveMessage event
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
+
+        public async Task AnotherMethod(string user, string message)
+        {
+            //Emits ReceiveMessage event
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+
     }
 }
